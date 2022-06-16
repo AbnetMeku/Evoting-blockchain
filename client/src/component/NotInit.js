121f67c9fc3abc1173580e2f8851753448205d5f
ABAYNEH TESSEMA <abnetmekuria73@gmail.com>
2022-06-16 18:55:31 +0300

    frontend added

---
+++ client/src/component/NotInit.js
@@ -0,0 +1,18 @@
+// Node module
+import React from "react";
+import "./NotInit.css";
+
+const NotInit = () => {
+  // Component: Displaying election not initialize message.
+  return (
+    <div  id="page">
+      <div className="container-item info">
+      <center>
+        <h3>The election has not been initialize.</h3>
+        <p>Please Wait..</p>
+      </center>  
+      </div>
+    </div>
+  );
+};
+export default NotInit;
